6204e4d041f8c470d0b8250cc1b2734a3d5912f9
James Wolff <james.wolff0@gmail.com>
2023-02-20 16:04:07 -0600

    Finished 'enums'

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,81 @@
+#[derive(Debug)]
+enum UsState{
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+    California,
+    Colorado,
+    Connecticut,
+    Delaware,
+    Florida,
+    Georgia,
+    Hawaii,
+    Idaho,
+    Illinois,
+    Indiana,
+    Iowa,
+    Kansas,
+    Kentucky,
+    Louisiana,
+    Maine,
+    Maryland,
+    Massachusetts,
+    Michigan,
+    Minnesota,
+    Mississippi,
+    Missouri,
+    Montana,
+    Nebraska,
+    Nevada,
+    New_Hampshire,
+    New_Jersey,
+    New_Mexico,
+    New_York,
+    North_Carolina,
+    North_Dakota,
+    Ohio,
+    Oklahoma,
+    Oregon,
+    Pennsylvania,
+    Rhode_Island,
+    South_Carolina,
+    South_Dakota,
+    Tennessee,
+    Texas,
+    Utah,
+    Vermont,
+    Virginia,
+    Washington,
+    West_Virginia,
+    Wisconsin,
+    Wyoming
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let coin = Coin::Quarter(UsState::Oklahoma);
+
+    println!("The value of coin is {}", value_in_cents(coin));
 }
