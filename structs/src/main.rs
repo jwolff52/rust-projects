134f3574b23d9afdd8a0e3f69be0321152c42aef
James Wolff <james.wolff0@gmail.com>
2023-02-20 21:25:08 +0000

    Added 'structs' project

---
+++ structs/src/main.rs
@@ -0,0 +1,59 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn is_square(&self) -> bool {
+        self.width == self.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!(
+        "The are of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let rect4 = Rectangle {
+        width: 45,
+        height: 45,
+    };
+    let sq = Rectangle::square(20);
+
+    println!("Is rect1 a square? {}", rect1.is_square());
+    println!("Is rect4 a square? {}", rect4.is_square());
+    println!("Is sq a square? {}", sq.is_square());
+}
