4107ada6fe26e882f67aadb7deb132d008fcba12
James Wolff <james.wolff0@gmail.com>
2023-02-23 16:23:51 +0000

    Finish restaurant example

---
+++ restaurant/src/back_of_house.rs
@@ -0,0 +1,20 @@
+#[derive(Debug)]
+pub enum Appetizer {
+    Soup,
+    Salad,
+}
+
+pub struct Breakfast {
+    pub toast: String,
+    seasonal_fruit: String,
+}
+
+impl Breakfast {
+    pub fn summer(toast: &str) -> Breakfast {
+        Breakfast { toast: String::from(toast), seasonal_fruit: String::from("peaches") }
+    }
+
+    pub fn seasonal_fruit(&self) -> &str {
+        &self.seasonal_fruit
+    }
+}
--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,26 +1,43 @@
-mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+mod front_of_house;
 
-        fn seat_at_table() {}
-    }
+mod back_of_house;
 
-    mod serving {
-        fn take_order() {}
+use crate::front_of_house::{hosting, serving};
 
-        fn serve_order() {}
+pub fn eat_at_restaurant() {
+    hosting::add_to_wait_list();
+    hosting::seat_at_table();
+    serving::take_order();
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast with {} please!", meal.toast, meal.seasonal_fruit());
 
-        fn take_payment() {}
-    }
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    println!("{:?}", order1);
+    println!("{:?}", order2);
+
+    serving::serve_order();
+    serving::take_payment();
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::front_of_house::hosting;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = hosting::add_to_wait_list();
+        assert_eq!(result, ());
+
+        let result = hosting::seat_at_table();
+        assert_eq!(result, ());
     }
 }
